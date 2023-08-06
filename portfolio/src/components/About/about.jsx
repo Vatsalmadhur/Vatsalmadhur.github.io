a4909223441b51c27ecf7dfd04865ddec77ae774
Madhur <bhartimadhur21@gmail.com>
2023-08-06 21:29:20 +0530

    updated fonts

--- portfolio/src/components/About/about.jsx
+++ portfolio/src/components/About/about.jsx
@@ -8,7 +8,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Sphere from "../Sphere/Sphere.jsx";
 import Forward from "../Navigation/Forward";
 import Navbar from "../Navbar/Navbar";
-import {gsap} from "gsap"
+import { gsap } from "gsap";
 
 function CCB() {
   const { camera, gl } = useThree();
@@ -28,46 +28,43 @@ function CCB() {
 
   const controls = new OrbitControls(camera, gl.domElement);
   useEffect(() => {
-
-   
-
-
     reqRef.current = requestAnimationFrame(animate);
     return () => {
       controls.dispose();
     };
   }, [camera, gl]);
-
- 
 }
 
 function About() {
-const tl= new gsap.timeline();
-  let abtH=useRef(null);
-  let abtB=useRef(null);
-  useEffect(()=>{
-    tl.from([abtH,abtB],1,{
-      opacity:0,
-      y:100,
-      skewY :10,
-      stagger:{
-        amount:0.4
-      }
-  
-    })
-
-  })
+  const tl = new gsap.timeline();
+  let abtH = useRef(null);
+  let abtB = useRef(null);
+  useEffect(() => {
+    tl.from([abtH, abtB], 1, {
+      opacity: 0,
+      y: 100,
+      skewY: 10,
+      stagger: {
+        amount: 0.4,
+      },
+    });
+  });
 
   return (
     <>
       <Navbar />
       <div className="boxForAbt">
-        <div className="head_abt_1"  >
-          <p className="abtText1" ref={el=>abtH=el} >About Me!</p>
-          <p className="abtText2" ref={el=>abtB=el}>
-           Hi! I'm Madhur Vatsal Bharti. Nice to meet you! <br />
-           I am a self-taught Web developer, a fast-learner and an aspiring app developer.I passionately combine good design, technology, and innovation in all my projects, which I like to accompany from the first idea to release. <br />
-            I don't like to define myself by the work i've done. I define myself by the work i want to do.
+        <div className="head_abt_1">
+          <p className="abtText1" ref={(el) => (abtH = el)}>
+            About Me!
+          </p>
+          <p className="abtText2" ref={(el) => (abtB = el)}>
+            Hi! I'm Madhur Vatsal Bharti. Nice to meet you! <br />
+            I am a self-taught Web developer, a fast-learner and an aspiring app
+            developer.I passionately combine good design, technology, and
+            innovation in all my projects, which I like to accompany from the
+            first idea to release. <br />I don't like to define myself by the
+            work i've done. I define myself by the work i want to do.
           </p>
         </div>
         <div className="head_abt_2">
--- portfolio/src/components/Sphere/Sphere.jsx
+++ portfolio/src/components/Sphere/Sphere.jsx
@@ -5,16 +5,15 @@ function Box() {
   const mesh = useRef();
   return (
     <>
-    <pointLight
-          args={[0xfaa500, 1, 100]}
-          position={[0, 10, 10]}
-          intensity={1.5}
-        />
+      <pointLight
+        args={[0xfaa500, 1, 100]}
+        position={[0, 10, 10]}
+        intensity={1.5}
+      />
 
-
-      <mesh ref={mesh} >
-        <sphereGeometry args={[6, 64, 64]}  />
-        <meshStandardMaterial color={'#faa500'} />
+      <mesh ref={mesh}>
+        <sphereGeometry args={[6, 64, 64]} />
+        <meshStandardMaterial color={"#faa500"} />
       </mesh>
     </>
   );
