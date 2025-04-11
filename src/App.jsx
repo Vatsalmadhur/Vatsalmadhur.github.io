18ccb7cc14bd90f0101630a5797d3e1c704f90ca
Madhur Bharti <madhur@Madhurs-MacBook-Pro.local>
2025-04-11 14:05:50 +0530

    Remove lazy in  Contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import Loading from "./components/Loader/Loading";
 import Navbar from "./components/Navbar/Navbar";
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import Home from '../src/components/Home/Home'
+// import About from "./components/About/about";
+import Contact from "./components/Contact/contact";
+// import Skills from "./components/Skills/Skills";
+// import Project from "./components/Projects/project";
+
 // const Home = React.lazy(() => import('./components/Home/Home.jsx'));
 const About = React.lazy(() => import("./components/About/about"));
 const Skills = React.lazy(() => import("./components/Skills/Skills"));
 const Project = React.lazy(() => import("./components/Projects/project"));
-const Contact = React.lazy(() => import("./components/Contact/contact"));
+// const Contact = React.lazy(() => import("./components/Contact/contact"));
 // const Navbar = React.lazy(() => import("./components/Navbar/Navbar"));
 
 function App() {
@@ -28,10 +33,13 @@ function App() {
                 <Route exact path="/skills" element={<Skills />} />
                 <Route exact path="/project" element={<Project />} />
                 <Route exact path="/contact" element={<Contact />} />
+
               </Routes>
             </Suspense>
+
           }
-        />
+/>
+
       </Routes>
     </>
   );
